cf4ecddaa10262c7e00b038bec1fafaab3b0a15c
Lana 'Deji <dejiolulana@gmail.com>
2022-01-07 09:18:15 -0700

    Kaggle Notebook | Google Data Analytics - Cyclistic: Case Study | Version 2

---
+++ google-data-analytics-cyclistic-case-study.r
@@ -0,0 +1,154 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "e401bb2e",
+   "metadata": {
+    "_execution_state": "idle",
+    "_uuid": "051d70d956493feee0c6d64651c6a088724dca2a",
+    "papermill": {
+     "duration": 0.016392,
+     "end_time": "2022-01-07T16:18:07.870383",
+     "exception": false,
+     "start_time": "2022-01-07T16:18:07.853991",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "## Introduction"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "30c44cd1",
+   "metadata": {
+    "papermill": {
+     "duration": 0.016126,
+     "end_time": "2022-01-07T16:18:07.902644",
+     "exception": false,
+     "start_time": "2022-01-07T16:18:07.886518",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "## Business Task"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "808ca3d9",
+   "metadata": {
+    "papermill": {
+     "duration": 0.015437,
+     "end_time": "2022-01-07T16:18:07.933599",
+     "exception": false,
+     "start_time": "2022-01-07T16:18:07.918162",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "## Prepare"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "30150b77",
+   "metadata": {
+    "papermill": {
+     "duration": 0.016045,
+     "end_time": "2022-01-07T16:18:07.965405",
+     "exception": false,
+     "start_time": "2022-01-07T16:18:07.949360",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "## Process"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "be89ea97",
+   "metadata": {
+    "papermill": {
+     "duration": 0.016331,
+     "end_time": "2022-01-07T16:18:07.997346",
+     "exception": false,
+     "start_time": "2022-01-07T16:18:07.981015",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "## Analyze"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "0d484079",
+   "metadata": {
+    "papermill": {
+     "duration": 0.015232,
+     "end_time": "2022-01-07T16:18:08.028330",
+     "exception": false,
+     "start_time": "2022-01-07T16:18:08.013098",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "## Share"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "ea58e486",
+   "metadata": {
+    "papermill": {
+     "duration": 0.014449,
+     "end_time": "2022-01-07T16:18:08.058012",
+     "exception": false,
+     "start_time": "2022-01-07T16:18:08.043563",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "## Act"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.0.5"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 3.545093,
+   "end_time": "2022-01-07T16:18:08.185147",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2022-01-07T16:18:04.640054",
+   "version": "2.3.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
